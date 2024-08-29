c0a54b0655095884e4e77763543c8ce49fd04172
Luis Jorge Alejandro Noriega De León <luisnoriega@CFPBARTOLOMEAMBROSIO.COM>
2024-08-29 12:18:25 -0600

    Se actualizaron los archivos de three.js

--- Modulo_2/Three_js/Ejemplos/Custom Drone/main.js
+++ Modulo_2/Three_js/Ejemplos/Custom Drone/main.js
@@ -1,8 +1,65 @@
+const motor = document.getElementById("motor");
+const helice = document.getElementById("helice");
+const camara = document.getElementById("camara");
+const base = document.getElementById("base");
+
+const array_motor = [
+    {id: 1, text: "motor 1", ruta: "./recursos/model/motor/Motor1.gltf"},
+    {id: 2, text: "motor 2", ruta: "./recursos/model/motor/Motor2.gltf"},
+    {id: 3, text: "motor 3", ruta: "./recursos/model/motor/Motor3.gltf"}
+];
+
+const objeto_base = [
+    {id: 1, text4: "base", ruta: "./recursos/model/base/Base.gltf"},
+
+]
+
+const array_helice = [
+    {id: 0, text2: "helice 1", ruta: "./recursos/model/helices/Helice1.gltf", name: "helice1"},
+    {id: 1, text2: "helice 2", ruta: "./recursos/model/helices/Helice2.gltf", name: "helice2"},
+    {id: 2, text2: "helice 3", ruta: "./recursos/model/helices/Helice3.gltf", name: "helice3"}
+];
+
+const array_camara = [
+    {id: 0, text3: "camara 1",ruta: "./recursos/model/cam/Cam1.gltf"},
+    {id: 1, text3: "camara 2",ruta: "./recursos/model/cam/cam2.gltf"},
+    {id: 2, text3: "camara 3",ruta: "./recursos/model/cam/cam3.gltf"}
+];
+
+array_camara.forEach(obj => {
+    const $option3 = document.createElement("option");
+    $option3.value = obj.id;
+    $option3.innerHTML = obj.text3;
+    camara.appendChild($option3);
+});
+
+objeto_base.forEach(obj => {
+    const $option3 = document.createElement("option");
+    $option3.value = obj.id;
+    $option3.innerHTML = obj.text4;
+    base.appendChild($option3);
+});
+
+
+array_helice.forEach(obj => {
+    const $option2 = document.createElement("option");
+    $option2.value = obj.id;
+    $option2.innerHTML = obj.text2;
+    helice.appendChild($option2);
+});
+
+array_motor.forEach(obj => {
+    const $option = document.createElement("option");
+    $option.value = obj.id;
+    $option.innerHTML = obj.text;
+    motor.appendChild($option);
+});
+
 var scene = new THREE.Scene();
 
-var camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 0.1, 50);
+var camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 0.1, 100);
 scene.add(camera);
-camera.position.set(-7, 5, 10);
+camera.position.set(0, 8, 10);
 camera.lookAt(new THREE.Vector3());
 
 // Configuración del renderer
@@ -38,94 +95,71 @@ const gltfLoader = new THREE.GLTFLoader(loadingManager);
 
 const droneParts = {
     motor: new THREE.Group(),
-    helices: new THREE.Group(),
+    helice: new THREE.Group(),
     base: new THREE.Group(),
     camaras: new THREE.Group(),
+    base: new THREE.Group(),
+
 };
 
-gltfLoader.load('./recursos/model/base/Base.gltf', function (gltf) {
-    droneParts.base.add(gltf.scene);
-}, undefined, function (error) {
-    console.error('Error al cargar el modelo Base:', error);
+motor.addEventListener("change", (event) => {
+    const selectedMotor = array_motor.find((e) => e.id == event.target.value);
+    if (selectedMotor) {
+        loadModels(selectedMotor.ruta, droneParts.motor);
+    }
+    droneParts.motor.clear();
+
 });
 
-gltfLoader.load('./recursos/model/cam/Cam1.gltf', function (gltf) {
-    gltf.scene.traverse(function (child) {
-        if (child.isMesh) {
-            child.name = 'Cam1';
-        }
+base.addEventListener("change", (event) => {
+    const selectedbase = objeto_base.find((e) => e.id == event.target.value);
+    if (selectedbase) {
+        loadModels(selectedbase.ruta, droneParts.base);
+    }
+    droneParts.base.clear();
+
+});
+
+camara.addEventListener("change", (event) => {
+    const selectedCamara = array_camara.find((e) => e.id == event.target.value);
+    if (selectedCamara) {
+        loadModels(selectedCamara.ruta, droneParts.camaras);
+    }
+    droneParts.camaras.clear();
+
+});
+
+
+helice.addEventListener("change", (event) => {
+    loadModels(array_helice.find((e) => e.id == event.target.value).ruta, droneParts.helice, 'helice3');
+    droneParts.helice.clear();
+
+});
+
+function loadModels(ruta, groupParts, name) {
+    gltfLoader.load(ruta, function (gltf) {
+        gltf.scene.traverse(function (child) {
+            if (child.isMesh) {
+                child.name = name;
+            }
+        });
+        groupParts.add(gltf.scene);
+    }, undefined, function (error) {
+        console.error('Error al cargar el modelo:', error);
     });
-    droneParts.helices.add(gltf.scene);
-}, undefined, function (error) {
-    console.error('Error al cargar el modelo Helice3:', error);
-});
+}
 
 
-gltfLoader.load('./recursos/model/motor/Motor2.gltf', function (gltf) {
-    droneParts.motor.add(gltf.scene);
-}, undefined, function (error) {
-    console.error('Error al cargar el modelo Motor2:', error);
-});
-
-gltfLoader.load('./recursos/model/helices/Helice2.gltf', function (gltf) {
-    gltf.scene.traverse(function (child) {
-        if (child.isMesh) {
-            child.name = 'helice2';
-        }
-    });
-    droneParts.helices.add(gltf.scene);
-}, undefined, function (error) {
-    console.error('Error al cargar el modelo Helice3:', error);
-});
+loadModels(array_motor[0].ruta, droneParts.motor, 'motor1');
+loadModels(array_helice[0].ruta, droneParts.helice, 'helice3');
+loadModels(array_camara[0].ruta, droneParts.camaras, 'cam1');
+loadModels(objeto_base[0].ruta, droneParts.base, 'base');
 
 scene.add(droneParts.base);
 scene.add(droneParts.camaras);
 scene.add(droneParts.motor);
-scene.add(droneParts.helices);
-
-const clock = new THREE.Clock()
-
-const animate = () => {
-    const elapsedTime = clock.getElapsedTime()
-
-    const movement = Math.sin(elapsedTime)
-    droneParts.base.position.y = movement * 0.3
-    droneParts.camaras.position.y = movement * 0.3
-    droneParts.motor.position.y = movement * 0.3
-    droneParts.helices.position.y = movement * 0.3
-
-    try {
-        for (
-            let i = 0;
-            i < droneParts.helices.children.length;
-            i++
-        ) {
-            droneParts.helices.traverse(function (child) {
-                if (child.name === 'helice2') {
-                    child.rotation.y += 0.1;
-                }});
-        }
-        for (
-            let i = 0;
-            i < droneParts.camaras.children.length;
-            i++
-        )
-        {
-            droneParts.camaras.traverse(function (child) {
-                if (child.name === 'Cam1') {
-                    child.rotation.x  += 0.5;
-                }});
-        }
-
-    } catch (error) { }
-    orbitControls.update()
-
-    //post processing render
-    renderer.render(scene, camera)
-    requestAnimationFrame(animate)
-}
-animate()
-
+scene.add(droneParts.helice);
+scene.add(droneParts.base);
 const plane = new THREE.Mesh(
     new THREE.PlaneBufferGeometry(10, 10),
     new THREE.ShadowMaterial({ opacity: 0.5 })
@@ -142,10 +176,102 @@ scene.background = new THREE.Color(0xfafaf9);
 const light1 = new THREE.DirectionalLight(0xfcfcfc, 4.3);
 light1.position.set(0, 6, 1);
 light1.castShadow = true;
-light1.shadow.mapSize.set(2048, 2048);
+light1.shadow.mapSize.set(2080, 2048);
 light1.shadow.bias = -0.000131;
 scene.add(light1);
 
 const al = new THREE.AmbientLight(0x208080, 0.61);
 scene.add(al);
 
+const clock = new THREE.Clock();
+
+// velocidad y la dirección del dron
+const moveSpeed = 0.1;
+let movingUp = false;
+let movingDown = false;
+let movingLeft = false;
+let movingRight = false;
+
+// detectar cuando se presiona una tecla
+window.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowUp" || e.code === "KeyW") {
+        movingUp = true;
+    } else if (e.key === "ArrowDown" || e.code === "KeyS") {
+        movingDown = true;
+    } else if (e.key === "ArrowLeft" || e.code === "KeyA") {
+        movingLeft = true;
+    } else if (e.key === "ArrowRight" || e.code === "KeyD") {
+        movingRight = true;
+    } 
+});
+
+// E detectar cuando se suelta una tecla
+window.addEventListener("keyup", (e) => {
+    if (e.key === "ArrowUp" || e.code === "KeyW") {
+        movingUp = false;
+    } else if (e.key === "ArrowDown" || e.code === "KeyS") {
+        movingDown = false;
+    } else if (e.key === "ArrowLeft" || e.code === "KeyA") {
+        movingLeft = false;
+    } else if (e.key === "ArrowRight" || e.code === "KeyD") {
+        movingRight = false;
+    } 
+});
+
+const animate = () => {
+    const elapsedTime = clock.getElapsedTime();
+
+    if (movingUp) {
+        if (droneParts.base.position.y < 1) {
+            // Bajar el dron 
+        droneParts.base.position.y += moveSpeed;
+        droneParts.camaras.position.y += moveSpeed;
+        droneParts.motor.position.y += moveSpeed;
+        droneParts.helice.position.y += moveSpeed;
+        }
+    }
+    
+
+    if (movingDown) {
+        if (droneParts.base.position.y > -1) {
+            // Bajar el dron 
+            droneParts.base.position.y -= moveSpeed;
+            droneParts.camaras.position.y -= moveSpeed;
+            droneParts.motor.position.y -= moveSpeed;
+            droneParts.helice.position.y -= moveSpeed;
+        } 
+    }
+    
+
+    if (movingLeft) {
+            // Mover el dron a la izquierda
+            droneParts.base.position.x -= moveSpeed;
+            droneParts.camaras.position.x -= moveSpeed;
+            droneParts.motor.position.x -= moveSpeed;
+            droneParts.helice.position.x -= moveSpeed;
+    }
+    
+
+    if (movingRight) {
+        droneParts.base.position.x += moveSpeed;
+        droneParts.camaras.position.x += moveSpeed;
+        droneParts.motor.position.x += moveSpeed;
+        droneParts.helice.position.x += moveSpeed;
+    }
+
+
+    // Rotar la hélice
+    droneParts.helice.traverse((child) => {
+        if (child.name === 'helice3') {
+            child.rotation.y += 0.3; 
+        }
+    });
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+
+
+
+};
+
+animate();
